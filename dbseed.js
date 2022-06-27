2dc5bec65fd13f8edd61c9d4363289e294b434b2
Jen Beckett <jen@rfdmsolutions.com>
2022-06-27 12:46:47 -0700

    updated DB info

--- dbseed.js
+++ dbseed.js
@@ -1,13 +1,17 @@
 //this imports the postgres connector into the file so it can be used
 const { Client } = require('pg');
+require('custom-env').env();
 
+
+console.log();
+console.log();
 //instantiates a client to connect to the database, connection settings are passed in
 const client = new Client({
-    user: 'postgres',
-    host: 'sespe-test-2.chtwubdr7alu.us-west-2.rds.amazonaws.com',
-    database: 'sespe-sync-1',
-    password: 'postgres',
-    port: 5555
+    user: process.env.POSTGRESQL_DB_USER,
+    host: POSTGRESQL_DB_HOST,
+    database: POSTGRESQL_DB,
+    password: POSTGRESQL_DB_PASSWORD,
+    port: process.env.POSTGRESQL_DB_PORT
 });
 
 //the lambda funtion code
